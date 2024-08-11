d898d5af99c87e45caac1fc7052efde44d3c09fb
Rodrigo-Sebastian <rodrigo.seb93@gmail.com>
2024-08-12 01:07:08 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
+/*
+/** @type {import('next').NextConfig} */
+/*const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig*/
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    // Lägg till regler för att ignorera testfiler
+    config.module.rules.push({
+      test: /\.test\.js$/,
+      use: 'ignore-loader',
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
